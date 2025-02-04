5af55291126e9186ff2848f3428b6447c5bf3724
ShinBeomki <97145511+ShinBeomki@users.noreply.github.com>
2025-02-04 14:56:05 +0900

    보안 스터디

--- server/src/main/java/study/api/board/service/BoardService.java
+++ server/src/main/java/study/api/board/service/BoardService.java
@@ -27,6 +27,11 @@ public class BoardService {
                 .build();
 
             Board savedBoard = boardRepository.save(board);
+            // application.yml파일에서 open-in-view 설정을 false로 하지않으면 (default값이 true임)
+            // api요청과 응답사이에 영속성이 계속 유지 되기 때문에 save한 내용이 find부분에서
+            // db를 거치지않고 1차 캐시에서 바로 리턴됨으로 join의 결과가 null로 표시 된다.
+
+            // - @Tranctional 어노테이션을 선언하고 EntityManager로 flush, clear로 명시적으로 캐시를 삭제 하는 방법도 있음
             Board boardWithMembers = boardRepository.findByIdWithMember(savedBoard.getId());
         return ResponseBoard.build(boardWithMembers);
     }
@@ -50,6 +55,7 @@ public class BoardService {
     // 수정
     @Transactional
     public ResponseBoard update(Long id ,RequestBoard req) {
+        // 헤더에서 멤버 아이디 취득해서 본인이 쓴 글인지 판단
         Board board = boardRepository.findById(id).get();
         board.updateBoard(req.getTitle(),req.getContent());
         return ResponseBoard.build(board);
@@ -58,6 +64,7 @@ public class BoardService {
     // 삭제 (SoftDelete)
     @Transactional
     public void delete(Long id) {
+        // 헤더에서 멤버 아이디 취득해서 본인이 쓴 글인지 판단
         boardRepository.deleteById(id);
     }
 }
